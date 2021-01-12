534d2b859c769ba0593fe0ceda7e0558f34234c7
Subham Sharma <33781108+mr-sharmaji@users.noreply.github.com>
2021-01-13 01:50:30 +0530

    connected to database to fix all the errors

--- heartRate.php
+++ heartRate.php
@@ -1,38 +1,37 @@
 <?php
 
 header('content-type: application/json');
+$con = pg_connect("host=ec2-54-144-196-35.compute-1.amazonaws.com port=5432 dbname=d1k8si0lv5fivd user=wsdkrljactirnp password=a4e7f74aa4c59e000467c2e0ca8db14a1fc13a801b57f43f3839cd5bc32ea362") or die ("Could not connect to server\n");
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
     $arr_data = array();
-    $myFile = "heartRateData.json";
     try {
-        $jsondata = file_get_contents($myFile);
-        $arr_data = json_decode($jsondata, true);
+        $query = "Select * from heartrate LIMIT 10;";
+        $result = pg_query($con, $query) or die("Cannot execute query: $query\n");
+        while ($row = pg_fetch_row($result)) {
+            $arr_data = ["heartRate" => $row[1]];
+          }
         echo json_encode($arr_data);
     } catch (Exception $e) {
-        echo json_encode("Caught exception");
+        echo json_encode(array('data'=>"Something Went Wrong!"));
     }
+    pg_close($con);
 }
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $arr_data = array();
-    $myFile = "heartRateData.json";
-    $t=time();
+    $data = json_decode(file_get_contents('php://input'), true);
     try {
-        $data = json_decode(file_get_contents('php://input'), true);
-        $formdata = array(
-            'heartRate' =>  $data['heartRate'],
-        );
-        $jsondata = file_get_contents($myFile);
-        $arr_data = json_decode($jsondata, true);
-        array_push($arr_data, $formdata);
-        $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
-        if (file_put_contents($myFile, $jsondata)) {
+        $dataHeartRate = $data['heartRate'];
+        $query = "INSERT INTO time(created_on) values ('$dataHeartRate');";
+        $result = pq_query($con, $query);
+        if ($result) {
             echo json_encode(array('data'=>"Data Successfully Added"));
         } else
-            echo json_encode(array('data'=>"ERROR"));
+            echo json_encode(array('data'=>"Something Went Wrong!"));
 
     } catch (Exception $e) {
-        echo json_encode("Caught exception");
+        echo json_encode(array('data'=>"Something Went Wrong!"));
     }
+    pg_close($con);
 }
